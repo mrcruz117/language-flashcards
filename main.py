625499f5884dcdc0d2ecd33900fab7fb4b4d2060
mrcruz117 <mrcruz1989@gmail.com>
2022-05-02 10:48:45 -0400

    flip timer is working and cards are being navigated

--- main.py
+++ main.py
@@ -7,23 +7,37 @@ BACKGROUND_COLOR = "#B1DDC6"
 data = pandas.read_csv("data/french_words.csv")
 to_learn = data.to_dict(orient="records")
 print(to_learn)
+current_card = {}
 
 
 # next card function
 def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
     print(current_card)
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=current_card["French"])
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_image)
+    flip_timer = window.after(3000, func=flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill='white')
+    canvas.itemconfig(card_word, text=current_card["English"], fill='white')
+    canvas.itemconfig(card_background, image=card_back_image)
 
 
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, func=flip_card)
+
 canvas = Canvas(width=800, height=526)
 card_front_image = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=card_front_image)
+card_back_image = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_image)
 card_title = canvas.create_text(400, 150, text="Title", font=("roboto", 32, "italic"))
 card_word = canvas.create_text(400, 263, text="word", font=("roboto", 40, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
@@ -36,7 +50,5 @@ unknown_button.grid(row=1, column=0)
 check_image = PhotoImage(file="images/right.png")
 known_button = Button(image=check_image, highlightthickness=0, command=next_card)
 known_button.grid(row=1, column=1)
-
-next_card()
 
 window.mainloop()
