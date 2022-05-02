64adc36e34f7f7a45110f60d4384ddb0c21278a8
mrcruz117 <mrcruz1989@gmail.com>
2022-05-02 10:00:45 -0400

    showing front card on canvas as expected

--- main.py
+++ main.py
@@ -6,4 +6,10 @@ window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+canvas = Canvas(width=800, height=526)
+card_front_image = PhotoImage(file="images/card_front.png")
+canvas.create_image(400, 263, image=card_front_image)
+canvas.config(bg=BACKGROUND_COLOR,highlightthickness=0)
+canvas.grid(row=0, column=0)
+
 window.mainloop()
